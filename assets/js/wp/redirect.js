4bb4caad7d3ed9567c48072ba5782a8116245855
Eric Greer <eric@apsis.io>
2018-11-25 01:19:30 -0500

    Adding relative pathing

--- assets/js/wp/redirect.js
+++ assets/js/wp/redirect.js
@@ -2,12 +2,12 @@
 ---
 // This attempts to emulate 301 redirect for pages of the old wordpress site
 var redirects = {
-  '?page_id=6': '/services/',
-  '?page_id=7': '/about/',
-  '?page_id=8': '/contact/',
-  '?page_id=9': '/links/',
-  '?page_id=16': '/news/',
-  '?p=62' : '/2013/01/02/tedx-talk-on-compassion/'
+  '?page_id=6': 'services/',
+  '?page_id=7': 'about/',
+  '?page_id=8': 'contact/',
+  '?page_id=9': 'links/',
+  '?page_id=16': 'news/',
+  '?p=62' : '2013/01/02/tedx-talk-on-compassion/'
 };
 (function() {
 var s = document.location.search;
--- news/index.md
+++ news/index.md
@@ -7,7 +7,7 @@ order: 4
 
 <div>
 {% for post in site.posts %}
-    <h3><a href="{{ post.url }}">{{ post.title }}</a></h3>
+    <h3><a href="{{ post.url | relative_url }}">{{ post.title }}</a></h3>
     {{ post.content }}
 {% endfor %}
 </div>
